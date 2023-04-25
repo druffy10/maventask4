644936b60c20f435a9552e57117f115926443b93
d1sam <d1sam.dot@gmail.com>
2023-04-25 14:13:16 +0300

    add test commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,15 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    @Test
+    void findMaxWithNegativeNumbers() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-12, -5, -8, -4, -5, -3, -8, -6, -11, -11, -12};
+        long expected = -3;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
